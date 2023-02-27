58d3248982e9604322e9abca42d73ef6ede31e5a
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-02-27 18:07:37 +0100

    Poprawa funkcji my_print i interakcji z programem

--- lab1/main.py
+++ lab1/main.py
@@ -4,19 +4,19 @@ import sys
 
 def my_printf(format_string,param):
     #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
+    shouldDo = True
+    for idx in range(0, len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
+            if format_string[idx] == '#' and format_string[idx + 1] == 'k':
+                print(param.swapcase().rstrip('\n'), end = "")
             else:
                 print(format_string[idx],end="")
         else:
             shouldDo=True
     print("")
 
-data=sys.stdin.readlines()
+format = input()  # sys.stdin.readlines()
+str = input()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+my_printf(format, str)
+
