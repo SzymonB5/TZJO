9eb1eaf46bef6a17dafefce460c58c65139a5014
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-02-27 18:32:49 +0100

    Poprawa funkcji oraz testów

--- lab1/main.py
+++ lab1/main.py
@@ -5,18 +5,22 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo = True
+    skip = True
     for idx in range(0, len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx + 1] == 'k':
                 print(param.swapcase().rstrip('\n'), end = "")
+                skip = False
             else:
-                print(format_string[idx],end="")
+            	if (skip):
+                	print(format_string[idx],end="")
+            	skip = True
         else:
             shouldDo=True
     print("")
-
-format = input()  # sys.stdin.readlines()
-str = input()
-
-my_printf(format, str)
+    
+for i in range(10):
+	format = input()  # sys.stdin.readlines()
+	str = input()
+	my_printf(format, str)
 
