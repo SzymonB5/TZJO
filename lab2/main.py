999bc932d313ed28dc3e64b7a97955dd80436ae5
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-03-06 16:55:45 +0100

    Dodanie pomocniczej funkcji separującej format_str

--- lab2/main.py
+++ lab2/main.py
@@ -2,6 +2,28 @@
 
 import sys
 
+
+def separateStr(format_string, param):
+    for i in range(0, len(format_string) - 1):
+        if format_string[i] == '#':
+            if format_string[i + 1] == 'k': 
+                return 1, format_string[0:i], -1, format_string[i + 2:len(format_string)]
+            if format_string[i + 1] == '.':
+            	if format_string[i + 2].isnumeric() == False:
+            	    continue
+            	else:
+            	    save = 0
+            	    for j in range(i + 2, len(format_string)):
+            	        if format_string[j].isnumeric() == False:
+            	            save = j
+            	            break
+            	            
+            	    if format_string[save] != 'k':
+            	        continue
+            	    return 2, format_string[0:i], int(format_string[i + 2:save]), format_string[save + 1:len(format_string)]        
+       
+    return 0, format_string, -1, -1    
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
@@ -16,8 +38,14 @@ def my_printf(format_string,param):
             shouldDo=True
     print("")
 
-data=sys.stdin.readlines()
+# data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
-    
+# for i in range(0,len(data),2):
+  #  my_printf(data[i].rstrip(),data[i+1].rstrip())
+  
+print(separateStr("test", "ignore"))
+print(separateStr("AA#kAA", "ignore"))
+print(separateStr("#.05k", "ignore"))
+print(separateStr("#", "ignore"))
+print(separateStr("test", "ignore"))
+
