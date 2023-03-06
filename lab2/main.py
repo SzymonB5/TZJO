3303346da64727b26a4aa73829a8b1fe3f6b6c65
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-03-06 17:09:37 +0100

    Stworzenie działającej funkcji my_printf

--- lab2/main.py
+++ lab2/main.py
@@ -26,26 +26,27 @@ def separateStr(format_string, param):
 
 def my_printf(format_string,param):
     #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(format_string,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+    typeOf, startFormat, number, endFormat = separateStr(format_string, param)
+    if typeOf == 0:
+    	print(format_string)
+    elif typeOf == 1:
+        print(startFormat, end="")
+        print(param.swapcase(), end="")
+        print(endFormat)
+    else:
+    	print(startFormat, end="")
+    	for i in range(0, min(len(param), number)):
+        	print(param[i].swapcase(), end="")
+    	print(endFormat)
 
 # data=sys.stdin.readlines()
 
 # for i in range(0,len(data),2):
   #  my_printf(data[i].rstrip(),data[i+1].rstrip())
   
-print(separateStr("test", "ignore"))
-print(separateStr("AA#kAA", "ignore"))
-print(separateStr("#.05k", "ignore"))
-print(separateStr("#", "ignore"))
-print(separateStr("test", "ignore"))
+my_printf("test", "ignore")
+my_printf("AA#kAA", "ignORe")
+my_printf("A#.0kA", "ignore")
+my_printf("--#.100K", "ignore")
+my_printf("#100k", "ignore")
 
