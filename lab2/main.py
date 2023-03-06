344647541e92152d4e351348a9b4b4ced6d9b2e5
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-03-06 17:35:08 +0100

    Popawa funkcji my_print

--- lab2/main.py
+++ lab2/main.py
@@ -35,8 +35,10 @@ def my_printf(format_string,param):
         print(endFormat)
     else:
     	print(startFormat, end="")
-    	for i in range(0, min(len(param), number)):
-        	print(param[i].swapcase(), end="")
+    	for i in range(0, number - len(param)):
+    		print(" ", end="")
+    	
+    	print(param.swapcase(), end="")
     	print(endFormat)
 
 data=sys.stdin.readlines()
@@ -44,8 +46,8 @@ data=sys.stdin.readlines()
 for i in range(0,len(data),2):
    my_printf(data[i].rstrip(),data[i+1].rstrip())
   
-# my_printf("test", "ignore")
-# my_printf("AA#kAA", "ignORe")
+# my_printf("--#.2k--", "ignore")
+# my_printf("AA#.7kAA", "qwert")
 # my_printf("A#.0kA", "ignore")
 # my_printf("--#.100K", "ignore")
 # my_printf("#100k", "ignore")
