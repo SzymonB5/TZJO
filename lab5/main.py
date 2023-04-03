cee02aad1f1676171417fc8800f3a5fdff54dec7
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-03 16:25:38 +0200

    Dodanie funkcji pomocniczej

--- lab5/main.py
+++ lab5/main.py
@@ -2,11 +2,34 @@
 
 import sys
 
+def transform_number(num):
+
+    num_str = str(num)
+    new_digits = []
+    
+    for i in range(len(num_str)):
+        digit = int(num_str[i])
+        
+        digit -= 1
+        
+        if digit == -1:
+            new_digits.append(9)
+          
+        else:
+            new_digits.append(digit)
+    
+    new_str = ''.join(map(str, new_digits)).zfill(len(num_str))
+    
+    return new_str
+
+
+
+
 def separateStr(format_string, param):
     for i in range(0, len(format_string) - 1):
         if format_string[i] == '#':
             if format_string[i + 1] == 'g':
-            	return 4, format_string[0:i], str(param), format_string[i+2:len(format_string)]
+            	return 4, format_string[0:i], transform_number(param), format_string[i+2:len(format_string)]
        
     return 0, format_string, -1, -1    
 
@@ -20,8 +43,11 @@ def my_printf(format_string, param):
         print(printWithoutZero(str(param)[::-1]), end="")
         print(endFormat)
 
-data=sys.stdin.readlines()
+# data=sys.stdin.readlines()
 
-for i in range(0,len(data), 2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+print(transform_number(1234))
+print(transform_number(10101))
+
+# for i in range(0,len(data), 2):
+  #   my_printf(data[i].rstrip(),data[i+1].rstrip())
     
