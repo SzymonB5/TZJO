6dbac9bb6c4ed8a50909980001cd9af366bfc37e
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-03 16:12:52 +0200

    Powrót do lab 4

--- lab5/main.py
+++ lab5/main.py
@@ -2,21 +2,37 @@
 
 import sys
 
-def my_printf(format_string,param):
+def printWithoutZero(numb):
+    n = str(numb)
+    if int(n) == 0:
+        return 0       
+        
+    fr = 0
+    while n[fr] == '0':
+        fr += 1
+        
+    return n[fr:len(n)]
+
+def separateStr(format_string, param):
+    for i in range(0, len(format_string) - 1):
+        if format_string[i] == '#':
+            if format_string[i + 1] == 'g':
+            	return 4, format_string[0:i], str(param), format_string[i+2:len(format_string)]
+       
+    return 0, format_string, -1, -1    
+
+def my_printf(format_string, param):
     #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+    typeOf, startFormat, number, endFormat = separateStr(format_string, param)
+    if typeOf == 0:
+    	print(format_string)
+    else:
+        print(startFormat, end="")
+        print(printWithoutZero(str(param)[::-1]), end="")
+        print(endFormat)
 
 data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
+for i in range(0,len(data), 2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
+    
