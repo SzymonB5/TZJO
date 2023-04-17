8210bfa41d9ae8bab1f303346b62014c09a84b2b
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-17 16:32:20 +0200

    Function to separate string implemented

--- lab6/main.py
+++ lab6/main.py
@@ -7,6 +7,29 @@ def alterTo(numb):
     return (numb * 9 + 1) % 10
 
 
+def separateStr(format_string, param):
+    for i in range(0, len(format_string) - 1):
+        if format_string[i] == '#':
+            if format_string[i + 1] == '.':
+                if len(format_string) == i + 2:
+                    continue
+                if not format_string[i + 2].isnumeric():
+                    continue
+                else:
+                    save = 0
+                    for j in range(i + 2, len(format_string)):
+                        if not format_string[j].isnumeric():
+                            save = j
+                            break
+
+                    if format_string[save] != 'd':
+                        continue
+                    return 1, format_string[0:i], int(format_string[i + 2: save]), \
+                        format_string[save + 1:len(format_string)]
+
+    return 0, format_string, -1, -1
+
+
 def my_printf(format_string, param):
     # print(format_string)
     shouldDo = True
