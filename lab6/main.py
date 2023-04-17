c092eaaeb6a0aa3ca24b5ee5043013133f264c2e
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-17 16:33:45 +0200

    My_printf altered to work with my implementation

--- lab6/main.py
+++ lab6/main.py
@@ -31,18 +31,17 @@ def separateStr(format_string, param):
 
 
 def my_printf(format_string, param):
-    # print(format_string)
-    shouldDo = True
-    for idx in range(0, len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx + 1] == 'k':
-                print(param, end="")
-                shouldDo = False
-            else:
-                print(format_string[idx], end="")
-        else:
-            shouldDo = True
-    print("")
+    typeOf, startFormat, number, endFormat = separateStr(format_string, param)
+    if typeOf == 0:
+        print(format_string)
+    else:
+        print(startFormat, end="")
+        for i in range(int(number) - len(str(param))):
+            print("0", end="")
+        for j in str(param):
+            print(alterTo(int(j)), end="")
+
+        print(endFormat)
 
 
 data = sys.stdin.readlines()
