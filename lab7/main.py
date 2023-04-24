d7c85a561f0fd420ad5dd97f9df7f0f40867b3cf
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-24 16:20:15 +0200

    Code refactorisation

--- lab7/main.py
+++ lab7/main.py
@@ -3,19 +3,32 @@
 import sys
 
 
+def alterTo(numb):
+    if numb.isnumeric():
+        return numb
+    return chr(ord(numb) + 6)
+
+
+def separateStr(format_string, param):
+    for i in range(0, len(format_string) - 1):
+        if format_string[i] == '#':
+            if format_string[i + 1] == 'j':
+                return 1, format_string[0:i], format_string[i:i + 2], format_string[i + 2: len(format_string)]
+
+    return 0, format_string, -1, -1
+
+
 def my_printf(format_string, param):
-    # print(format_string)
-    shouldDo = True
-    for idx in range(0, len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx + 1] == 'k':
-                print(param, end="")
-                shouldDo = False
-            else:
-                print(format_string[idx], end="")
-        else:
-            shouldDo = True
-    print("")
+    typeOf, startFormat, number, endFormat = separateStr(format_string, param)
+    if typeOf == 0:
+        print(format_string)
+    else:
+        print(startFormat, end="")
+        print("0x", end="")
+        for j in str(hex(param)[2:]):
+            print(alterTo(j), end="")
+
+        print(endFormat)
 
 
 data = sys.stdin.readlines()
