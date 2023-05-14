4e14baa45bb85284b1c69ddf8a5510b18cd343a8
SzymonB5 <235831@edu.p.lodz.pl>
2023-05-14 22:59:13 +0200

    Lab 1 v2

--- lab1/main.py
+++ lab1/main.py
@@ -2,25 +2,26 @@
 
 import sys
 
-def my_printf(format_string,param):
-    #print(format_string)
+
+def my_printf(format_string, param):
+    # print(format_string)
     shouldDo = True
     skip = True
     for idx in range(0, len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx + 1] == 'k':
-                print(param.swapcase().rstrip('\n'), end = "")
+                print(param.swapcase().rstrip('\n'), end="")
                 skip = False
             else:
-            	if (skip):
-                	print(format_string[idx],end="")
-            	skip = True
+                if (skip):
+                    print(format_string[idx], end="")
+                skip = True
         else:
-            shouldDo=True
+            shouldDo = True
     print("")
-    
-for i in range(10):
-	format = input()  # sys.stdin.readlines()
-	str = input()
-	my_printf(format, str)
 
+
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+   my_printf(data[i].rstrip(),data[i+1].rstrip())
