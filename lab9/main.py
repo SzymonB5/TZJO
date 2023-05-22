fb5812cf1808854d95abd1ef3fe5c6d7674678b4
SzymonB5 <235831@edu.p.lodz.pl>
2023-05-22 17:32:14 +0200

    Dodanie funkcjonalności separowania stringu

--- lab9/main.py
+++ lab9/main.py
@@ -2,19 +2,45 @@
 
 import sys
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+
+def separateStr(format_string, param):
+    for i in range(0, len(format_string) - 1):
+        if format_string[i] == '#':
+            if format_string[i + 1] == 'k':
+                return 1, format_string[0:i], -1, format_string[i + 2:len(format_string)]
+            if format_string[i + 1] == '.':
+                if format_string[i + 2].isnumeric() == False:
+                    continue
+                else:
+                    save = 0
+                    for j in range(i + 2, len(format_string)):
+                        if format_string[j].isnumeric() == False:
+                            save = j
+                            break
+
+                    if format_string[save] != 'k':
+                        continue
+                    return 2, format_string[0:i], int(format_string[i + 2:save]), format_string[
+                                                                                  save + 1:len(format_string)]
+
+    return 0, format_string, -1, -1
+
+
+def my_printf(format_string, param):
+    # print(format_string)
+    typeOf, startFormat, number, endFormat = separateStr(format_string, param)
+    if typeOf == 0:
+        print(format_string)
+    elif typeOf == 1:
+        print(startFormat, end="")
+        print(param.swapcase(), end="")
+        print(endFormat)
+    else:
+        print(startFormat, end="")
+        for i in range(0, min(len(param), number)):
+            print(param[i].swapcase(), end="")
+        print(endFormat)
+
 
 data=sys.stdin.readlines()
 
