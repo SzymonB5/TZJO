15cbf58d470e741e807696a14a2c46760bdc71de
SzymonB5 <235831@edu.p.lodz.pl>
2023-05-29 17:46:50 +0200

    Created my_printf

--- lab10/main.py
+++ lab10/main.py
@@ -3,12 +3,40 @@
 import sys
 
 
+def count_digits(number):
+    if number == 0:
+        return 1
+
+    count = 0
+    number = abs(number)
+
+    while number > 0:
+        number //= 10
+        count += 1
+
+    return count
+
+
+def toHex(number):
+    return hex(number)
+
+
+def alter_param(number):
+    N = count_digits(number)
+    F = int((number * 2) / N)
+    print(f'F={F}')
+    if F % 2:
+        toHex(F)
+
+    return F
+
+
 def my_printf(format_string, param):
     shouldDo = True
     for idx in range(0, len(format_string) - 1):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx + 1] == 'd':
-                print(param, end="")
+            if format_string[idx] == '#' and format_string[idx + 1] == 'a':
+                print(alter_param(param), end="")
                 shouldDo = False
             else:
                 print(format_string[idx], end="")
@@ -17,7 +45,8 @@ def my_printf(format_string, param):
     print("")
 
 
-my_printf("#d", 2137)
+for i in range(0, 5000, 1):
+    my_printf("#a", i)
 
 # data = sys.stdin.readlines()
 #
